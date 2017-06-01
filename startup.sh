f54c73f2b6c6c604c944a98835b0fbd0cba57e45
Yaowenjie <wjyao@thoughtworks.com>
2017-06-01 11:01:26 +0800

    fix wrong path

--- startup.sh
+++ startup.sh
@@ -2,9 +2,9 @@
 echo 'removing old config file'
 rm -rf /etc/nginx/conf.d/*
 echo 'copy nginx conf file'
-mv /app/nginx.conf /etc/nginx/nginx.conf
+mv ./nginx.conf /etc/nginx/nginx.conf
 echo 'generating backend conf file'
-mv /conf/* /etc/nginx/conf.d/
+mv ./conf/* /etc/nginx/conf.d/
 service nginx restart
 echo 'starting nginx'
 exec "$@"
