239bfa1cec3b980aeabba469ac96947b6278b60b
Yaowenjie <wjyao@thoughtworks.com>
2017-06-01 12:55:36 +0800

    update script

--- startup.sh
+++ startup.sh
@@ -2,9 +2,9 @@
 echo 'removing old config file'
 rm -rf /etc/nginx/conf.d/*
 echo 'copy nginx conf file'
-mv ./nginx.conf /etc/nginx/nginx.conf
+cp -if ./nginx.conf /etc/nginx/nginx.conf
 echo 'generating backend conf file'
-mv ./conf/* /etc/nginx/conf.d/
+cp ./conf/* /etc/nginx/conf.d/
 service nginx restart
 echo 'starting nginx'
 exec "$@"
