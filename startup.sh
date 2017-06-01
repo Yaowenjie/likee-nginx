3d9c565a2df133da538426e20fe499e28a0cbdab
Yaowenjie <wjyao@thoughtworks.com>
2017-06-01 13:53:03 +0800

    split 80 and 443 as two server

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 echo 'removing old config file'
 rm -rf /etc/nginx/conf.d/*
 echo 'copy nginx conf file'
-cp -if ./nginx.conf /etc/nginx/nginx.conf
+cp -f ./nginx.conf /etc/nginx/nginx.conf
 echo 'generating backend conf file'
 cp ./conf/* /etc/nginx/conf.d/
 service nginx restart
