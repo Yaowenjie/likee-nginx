f8c85f151ae15f8f5b40bacc148a343fed16ddf7
Yaowenjie <wjyao@thoughtworks.com>
2017-06-01 10:45:14 +0800

    initial commit

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+echo 'removing old config file'
+rm -rf /etc/nginx/conf.d/*
+echo 'copy nginx conf file'
+mv /app/nginx.conf /etc/nginx/nginx.conf
+echo 'generating backend conf file'
+mv /conf/* /etc/nginx/conf.d/
+service nginx restart
+echo 'starting nginx'
+exec "$@"
